6f969f3e03e13ae76a3d0e5eaea8c92cdd21aeda
Nick Gorse <nick@olb42.com>
2021-04-09 15:34:55 +0100

    adding mvo function

--- zsh/fpath.zsh
+++ zsh/fpath.zsh
@@ -1,6 +1,16 @@
 #fpath+=
 typeset -gU fpath
+typeset -U function_files
 #add each topic folder to fpath so that they can add functions and completion scripts
-for topic_folder ($ZSH/bin) if [ -d $topic_folder ]; then  fpath=($topic_folder $fpath); fi;
+for topic_folder ($ZSH/bin) 
+    if [ -d $topic_folder ]; then  
+        fpath=($topic_folder $fpath);
+    fi;
+
+functions_files=($ZSH/bin/*(:t))
+for func in $functions_files
+    if typeset -f $func > /dev/null; then
+        unset -f $func;
+    fi;    
 
 autoload -U $ZSH/bin/*(:t)
