f791b4507e0836645742d1c3f6fd60a0f2312e18
Nick Gorse <nick@olb42.com>
2021-04-28 12:29:56 +0100

    nodenv

---
+++ zsh/autocd.zsh
@@ -0,0 +1,1 @@
+setopt  autocd autopushd pushdignoredups
--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,3 +1,7 @@
+export EDITOR='vim'
+export VISUAL='vim'
+
+
 # Source Prezto.
 if [[ -s "${ZDOTDIR:-$HOME}/.zprezto/init.zsh" ]]; then
    source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
