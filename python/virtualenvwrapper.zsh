e6f9e9e154a8f62de59ec73021b525abb80eeea4
Nick Gorse <nick@olb42.com>
2021-05-16 21:53:45 +0100

    adding old mac

--- python/virtualenvwrapper.zsh
+++ python/virtualenvwrapper.zsh
@@ -1,2 +1,18 @@
-export WORKON_HOME=~/.virtualenvs
-source $HOME/.local/bin/virtualenvwrapper.sh
+case `uname` in
+  Darwin)
+    # commands for OS X go here
+    eval "$(pyenv init -)"
+    eval "$(pyenv virtualenv-init -)"
+  ;;
+  Linux)
+    # commands for Linux go here
+    export PYTHONDONTWRITEBYTECODE=1  # don't write .pyc files
+    export WORKON_HOME=~/.virtualenvs
+    source $HOME/.local/bin/virtualenvwrapper.sh
+  ;;
+  FreeBSD)
+    # commands for FreeBSD go here
+  ;;
+esac
+
+
