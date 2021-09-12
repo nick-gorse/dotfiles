7e9e741604ce83de32bd42c21dfa5e4f65c486d0
nick@olb42.com <>
2021-09-13 00:54:36 +0100

    os_setup_start

---
+++ macos/set_defaults.sh
@@ -0,0 +1,6 @@
+# Always open everything in Finder's list view. This is important.
+defaults write com.apple.Finder FXPreferredViewStyle Nlsv
+
+# Run the screensaver if we're in the bottom-left hot corner.
+defaults write com.apple.dock wvous-bl-corner -int 10
+defaults write com.apple.dock wvous-bl-modifier -int 0
