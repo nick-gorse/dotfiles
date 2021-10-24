cc3250b21cb640e4cd34e4536dff57709e159021
nick-gorse <nick@olb42.com>
2021-10-24 19:40:32 +0100

    fix dotfiles history

--- zsh/fpath.zsh
+++ zsh/fpath.zsh
@@ -2,20 +2,22 @@
 typeset -gU fpath
 typeset -U function_files
 #add each topic folder to fpath so that they can add functions and completion scripts
-bin_files=($ZSH/bin)
-for topic_folder in $bin_files; do
-    if [ -d $topic_folder ]; then
-        fpath=($topic_folder $fpath)
-        typeset -gU fpath
-        echo $fpath >>$outfile
-    fi
-done
+# bin_files=($ZSH/bin)
+# for topic_folder in $bin_files; do
+#     if [ -d $topic_folder ]; then
+#         fpath=($topic_folder $fpath)
+#         typeset -gU fpath
+#     fi
+# done
+for topic_folder ($ZSH/*) if [ -d $topic_folder ]; then  fpath=($topic_folder $fpath); fi;
 
-functions_files=($ZSH/bin/*(:t))
-for func in $functions_files; do
-    if typeset -f $func >/dev/null; then
-        unset -f $func
-    fi
-done
+# functions_files=($ZSH/bin/*(:t))
+# for func in $functions_files; do
+#     if typeset -f $func >/dev/null; then
+#         unset -f $func
+#     fi
+# done
+#
+# autoload -U $functions_files
 
-autoload -U $functions_files
+
