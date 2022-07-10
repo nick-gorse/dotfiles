1cc6acf0ca6826c9eb8eb9356509476031e73120
nick-gorse <nick@olb42.com>
2022-07-10 18:22:58 +0100

    go-path-per-os

--- go/config.zsh
+++ go/config.zsh
@@ -1,4 +1,17 @@
+case $(uname) in
+Darwin)
+	# commands for OS X go here
+	export GOROOT=/usr/local/Cellar/go/1.17.2/libexec
+	;;
+Linux)
+	# commands for Linux go here
+	export GOROOT=$HOME/.go
+	;;
+FreeBSD)
+	# commands for FreeBSD go here
+	;;
+esac
 export GOPATH=$HOME/go # don't forget to change your path correctly!
-export GOROOT=/usr/local/Cellar/go/1.17.2/libexec
+
 export PATH=$PATH:$GOPATH/bin
 export PATH=$PATH:$GOROOT/bin
