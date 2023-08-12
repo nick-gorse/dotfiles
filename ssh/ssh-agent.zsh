5e506cd9548f906a9da24fa4ddc1c2805da698c2
nick-gorse <nick@olb42.com>
2023-08-12 22:10:01 +0100

    ssh-agent add

--- ssh/ssh-agent.zsh
+++ ssh/ssh-agent.zsh
@@ -3,8 +3,9 @@ if [[ -e $HOME/.1password/agent.sock ]]; then
     export SSH_AUTH_SOCK=$HOME/.1password/agent.sock
 else;
     runcount=$(ps -ef | grep "ssh-agent" | grep -v "grep" | wc -l)
-    if [ $runcount -eq 0 ]; then
-        eval $(ssh-agent -s)
-        ssh-add $HOME/.ssh/id_ed25519
+    if [ $runcount -ne 0 ]; then
+        killall -9 ssh-agent
     fi
+    eval $(ssh-agent -s)
+    ssh-add $HOME/.ssh/id_ed25519
 fi
