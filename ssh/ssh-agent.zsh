d80096745d4ace2c5edd7f70530c880d39ebe9ea
nick-gorse <nick@olb42.com>
2023-08-12 22:37:38 +0100

    ssh no agent

--- ssh/ssh-agent.zsh
+++ ssh/ssh-agent.zsh
@@ -1,11 +1,11 @@
 # SSH Agent should be running, once
 if [[ -e $HOME/.1password/agent.sock ]]; then 
     export SSH_AUTH_SOCK=$HOME/.1password/agent.sock
-else;
-    runcount=$(ps -ef | grep "ssh-agent" | grep -v "grep" | wc -l)
-    if [ $runcount -ne 0 ]; then
-        killall -9 ssh-agent
-    fi
-    eval $(ssh-agent -s &> /dev/null)
-    ssh-add $HOME/.ssh/id_ed25519 &> /dev/null
+# else;
+#     runcount=$(ps -ef | grep "ssh-agent" | grep -v "grep" | wc -l)
+#     if [ $runcount -ne 0 ]; then
+#         killall -9 ssh-agent
+#     fi
+#     eval $(ssh-agent -s &> /dev/null)
+#     ssh-add $HOME/.ssh/id_ed25519 &> /dev/null
 fi
