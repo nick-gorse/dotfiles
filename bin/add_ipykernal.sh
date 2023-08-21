66c901b46e146a1f6f86f65e9356475f63fcc371
nick-gorse <nick@olb42.com>
2023-08-21 11:35:53 +0100

    add_kernel updates

--- bin/add_ipykernal.sh
+++ bin/add_ipykernal.sh
@@ -1,5 +1,8 @@
 #!/bin/zsh
+
 if [[ -n $VIRTUAL_ENV && ! -f "./$VENV_FILE_EXTENSION" ]]; then
-    [[ `python -m ipykernel -V &> /dev/null` ]] || $VIRTUAL_ENV/bin/python -m pip install ipykernel
-    $VIRTUAL_ENV/bin/python -m ipykernel install --user --name=$(basename "${VIRTUAL_ENV//%\/.venv/}")
+    if [[ ! -e $(JUPYTER_PREFER_ENV_PATH=0 jupyter --data-dir)"/kernels/"$(basename "${VIRTUAL_ENV//%\/.venv/}") ]]; then
+        [[ `$VIRTUAL_ENV/bin/python -m ipykernel -V &> /dev/null` ]] || $VIRTUAL_ENV/bin/python -m pip install ipykernel
+        $VIRTUAL_ENV/bin/python -m ipykernel install --user --name=$(basename "${VIRTUAL_ENV//%\/.venv/}")
+    fi
 fi
