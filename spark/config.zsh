309b1dfa3d8cffb0b5c325da44f7009f557fb319
nick-gorse <nick@olb42.com>
2023-11-05 23:49:12 +0000

    variable log spark

--- spark/config.zsh
+++ spark/config.zsh
@@ -5,16 +5,20 @@ if which java > /dev/null; then export JAVA_HOME=$(/usr/libexec/java_home); fi
 # For a ipython notebook and pyspark integration
 if which pyspark > /dev/null; then
   export SPARK_VERSION="3.4.1"
+  echo "---- start spark config -----" >> $outfile
   SPARK_HOME="$(brew --cellar apache-spark)/$SPARK_VERSION/libexec"
 
-  if [[ -d $SPARK_HOME ]]; then
+  if [[ -e $SPARK_HOME ]]; then
+    echo "SPARK_HOME is set to ${SPARK_HOME} ;" >> $outfile
     export SPARK_HOME
   fi
   SPARK_BIN="$(brew --cellar apache-spark)/$SPARK_VERSION/bin"
-  if [[ -d $SPARK_BIN ]]; then
+  if [[ -e $SPARK_BIN ]]; then
+    echo "SPARK_HOME is set to ${SPARK_BIN}" >> $outfile
     export PATH=$SPARK_BIN:$PATH
   fi
   unset SPARK_BIN
+  echo "---- finish spark config -----" >> $outfile
   # export PYTHONPATH=$SPARK_HOME/python:$SPARK_HOME/python/build:$PYTHONPATH
   # export PYTHONPATH=$SPARK_HOME/python/lib/py4j-0.10.9.7-src.zip:$PYTHONPATH
 fi
