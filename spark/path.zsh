f32e46e59a6ab4a112538e60779466fc2ee60960
nick-gorse <nick@olb42.com>
2023-11-05 22:10:29 +0000

    spark changes

--- spark/path.zsh
+++ spark/path.zsh
@@ -1,9 +1,10 @@
-export SPARK_VERSION="3.4.1"
+
 
 if which java > /dev/null; then export JAVA_HOME=$(/usr/libexec/java_home); fi
 
 # For a ipython notebook and pyspark integration
 if which pyspark > /dev/null; then
+  export SPARK_VERSION="3.4.1"
   SPARK_HOME="$(brew --cellar apache-spark)/$SPARK_VERSION/libexec"
 
   if [[ -d $SPARK_HOME ]]; then
@@ -13,10 +14,11 @@ if which pyspark > /dev/null; then
   if [[ -d $SPARK_BIN ]]; then
     export PATH=$SPARK_BIN:$PATH
   fi
+  unset SPARK_BIN
   # export PYTHONPATH=$SPARK_HOME/python:$SPARK_HOME/python/build:$PYTHONPATH
   # export PYTHONPATH=$SPARK_HOME/python/lib/py4j-0.10.9.7-src.zip:$PYTHONPATH
 fi
-unset SPARK_BIN
+
 # export PYSPARK_PYTHON=/usr/local/bin/python3
 # export PYSPARK_DRIVER_PYTHON=jupyter
 # export PYSPARK_DRIVER_PYTHON_OPTS=’notebook’
