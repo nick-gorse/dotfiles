931cd2bfd58c06d9f8d82094ec30b5d6d313f294
nick-gorse <nick@olb42.com>
2023-11-06 00:23:37 +0000

    homebrew library logging

--- 00-homebrew/config.zsh
+++ 00-homebrew/config.zsh
@@ -14,20 +14,23 @@ FreeBSD)
     ;;
 esac
 if [[ -e ${foo[$CPUTYPE]}/bin/brew ]]; then
-    echo "\nEnvironment Variables set for homebrew" >>$outfile
+    echo "\n----- Start Environment Variables set for homebrew" >>$outfile
     echo "$(${foo[$CPUTYPE]}/bin/brew shellenv)" >>$outfile
     eval "$(${foo[$CPUTYPE]}/bin/brew shellenv)"
+    echo "----- End Environment Variables set for homebrew" >>$outfile
 fi
 declare -a LD_F CPP_F PKG_F
-brew_libs=(xz readline zlib bzip2 openssl@1.1 tcl-tk openblas lapack openjdk@11)
+brew_libs=(xz readline zlib bzip2 openssl@1.1 openssl@3 tcl-tk openblas lapack openjdk@11)
 LD_PREFIX="-L" LD_SUFFIX="/lib" CPP_PREFIX="-I" CPP_SUFFIX="/include" PKG_SUFFIX="/lib/pkgconfig"
 LD_F+=`echo ${LD_PREFIX}$(brew --prefix )${LD_SUFFIX}`
 CPP_F+=`echo ${CPP_PREFIX}$(brew --prefix "${library}")${CPP_SUFFIX}`
+echo "----- Start Adding Libraries from homebrew" >>$outfile
 for library in ${(@)brew_libs}; do
     if [[ -d $(brew --prefix "${library}") ]]; then
         LD_F+=`echo ${LD_PREFIX}$(brew --prefix "${library}")${LD_SUFFIX}`
         CPP_F+=`echo ${CPP_PREFIX}$(brew --prefix "${library}")${CPP_SUFFIX}`
         [[ -d `echo $(brew --prefix "${library}")${PKG_SUFFIX}` ]] && PKG_F+=`echo $(brew --prefix "${library}")${PKG_SUFFIX}`
+        echo "added library '`echo $library`' from brew into [LD, CPP and PKG]" >> $outfile
     fi
 done
 unset LD_PREFIX LD_SUFFIX CPP_PREFIX CPP_SUFFIX PKG_SUFFIX brew_libs
@@ -38,4 +41,5 @@ fi
 export LDFLAGS="${LDFLAGS}${LDFLAGS:+ }${LD_F}"
 export CPPFLAGS="${CPP_FLAGS}${CPP_FLAGS:+ }${CPP_F}"
 export PKG_CONFIG_PATH="${PKG_CONFIG_PATH:+:}${PKG_F}"
+echo "----- End Adding Libraries from homebrew" >>$outfile
 unset CPP_F LD_F PKG_F
--- python/01-pyenv-config.zsh
+++ python/01-pyenv-config.zsh
@@ -2,6 +2,6 @@ export PYENV_VIRTUALENV_DISABLE_PROMPT=1
 export PYENV_ROOT="$HOME/.pyenv"
 if command -v pyenv &> /dev/null; then 
 	eval "$(pyenv init -)"
-	if command -v pyenv-virtualenv-init &> /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
 	eval "$(pyenv init --path)"
+	if command -v pyenv-virtualenv-init &> /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
 fi
