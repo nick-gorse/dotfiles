ced99259a6865724753c21d074c4c52c1dff5e57
nick-gorse <nick@olb42.com>
2025-11-05 16:24:11 +0000

    pre refactored changes from chat

--- zplug/config.zsh
+++ zplug/config.zsh
@@ -4,12 +4,12 @@ export ZPLUG_LOADFILE=${DOTFILES}/zplug/packages
 if [[ ! -d $HOME/.zplug ]]; then
     export ZPLUG_HOME=$HOME/.zplug
     git clone https://github.com/zplug/zplug $ZPLUG_HOME
-    source $HOME/.zplug/init.zsh
+    call_file "$HOME/.zplug/init.zsh" "zplug"
     zplug 'zplug/zplug', hook-build:'zplug --self-manage'
     zplug install &> /dev/null 
     zplug update &> /dev/null
 else;
-    source $HOME/.zplug/init.zsh
+    call_file "$HOME/.zplug/init.zsh" "zplug"
     zplug 'zplug/zplug', hook-build:'zplug --self-manage'
     if ! zplug check; then
         zplug install &> /dev/null
