10e5cd486f2ced97043dda2f54004a9a22faa862
nick-gorse <nick@olb42.com>
2026-02-13 22:51:38 +0000

    Prevent creation of .DS_Store on network volumes
    Disable creation of .DS_Store files on network-mounted volumes by
    setting the `DSDontWriteNetworkStores` preference to true. This avoids
    cluttering network shares with Finder metadata when configuring macOS
    defaults via the set_defaults.sh script.

--- macos/set_defaults.sh
+++ macos/set_defaults.sh
@@ -6,3 +6,4 @@ defaults write com.apple.dock wvous-bl-corner -int 10
 defaults write com.apple.dock wvous-bl-modifier -int 0
 
 # defaults write digital.twisted.noTunes replacement /Applications/Spotify.app
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool TRUE
