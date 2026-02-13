eb4495a93a57763bdf9387e275688789b813f687
nick-gorse <nick@olb42.com>
2026-02-13 18:45:39 +0000

    Use long format for eza by default
    Change the default eza parameters to use the long listing format
    instead of the compact grid. This enables full file details (mode,
    links, owner, size, timestamp) by default while keeping other
    customizations like icons, git integration, and hyperlinks.

--- eza/env.zsh
+++ eza/env.zsh
@@ -1,1 +1,1 @@
-export eza_params=(--grid --git-ignore --all --header --grid --no-permissions --no-filesize --no-time --no-user --icons --git --hyperlink)
+export eza_params=(--grid --git-ignore --all --header --grid --long --no-permissions --no-filesize --no-time --no-user --icons --git --hyperlink)
